650f564a21ab3750671d68c08d77f71553f1c2ea
weiguang <548544375@qq.com>
2016-10-10 23:19:15 +0800

    buff的操作方法

--- src/helloworld.go
+++ src/helloworld.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"math"
 	"sbuffer"
 )
 
@@ -24,32 +22,4 @@ func main() {
 	fmt.Printf("%v", test)
 }
 
-type MyBuff struct {
-	bytes.Buffer
-}
 
-type Vertex struct {
-	x, y float64
-}
-
-func (v *Vertex) Scale(f float64) {
-	v.x = v.x * f
-	v.y = v.y * f
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.x * v.x + v.y * v.y)
-}
-
-type ErrNegativeSqrt float64
-
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("unsupported number.%v", float64(e))
-}
-
-func sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
-	}
-	return math.Sqrt(x), nil
-}
