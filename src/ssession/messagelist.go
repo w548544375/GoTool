dce71fd9307a92508c16e3b05f3065ba3245c0c2
weiguang <548544375@qq.com>
2016-10-12 23:26:12 +0800

    错误处理

---
+++ src/ssession/messagelist.go
@@ -0,0 +1,38 @@
+package ssession
+
+import (
+	"smessage"
+	"sync"
+)
+
+type MessageList struct {
+	//
+	list []*SSession
+
+	guard sync.Mutex
+}
+
+//放入消息
+func (self *MessageList) Push(msg *smessage.SMessage) {
+	self.guard.Lock()
+	defer self.guard.Unlock()
+	self.list = append(self.list, msg)
+}
+
+//取消息
+func (self *MessageList) Pick() *smessage.SMessage {
+	self.guard.Lock()
+	defer self.guard.Unlock()
+	if len(self.list) == 0 {
+		return nil
+	}
+	message := self.list[0]
+	self.list = self.list[1:]
+	return message
+}
+
+func (self *MessageList) Reset() {
+	self.guard.Lock()
+	defer self.guard.Unlock()
+	self.list = self.list[0:0]
+}
